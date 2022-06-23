e1db2508cb366af84c5a5a8673139f3096e3281b
FranciscoSimonWasel <69538183+FranciscoSimonWasel@users.noreply.github.com>
2022-06-22 22:52:53 -0500

    entities

---
+++ src/main/java/com/anahuac/calidad/tareaUnitTest/entities/BookShelfCapacityReached.java
@@ -0,0 +1,8 @@
+package com.anahuac.calidad.tareaUnitTest.entities;
+
+
+public class BookShelfCapacityReached extends RuntimeException {
+    public BookShelfCapacityReached(String message) {
+        super(message);
+    }
+}
